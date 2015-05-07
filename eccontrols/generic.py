kmorel/EntertainmentCenterServer

commit 00234e4e2e31452c4ff1c74d2e786b03707691e3

Change icon and splash screen.

eccontrols/generic.py
@@ -11,10 +11,10 @@ class GenericIR:
     commands = {}
 
     def __init__(self, device):
-        if not device in lirc.devices():
-            raise ECError('No such device: %s' % device)
+        # if not device in lirc.devices():
+        #     raise ECError('No such device: %s' % device)
         self.device = device
-        self.commands = lirc.commands(device)
+        # self.commands = lirc.commands(device)
 
     def send(self, command):
         """Send an IR code. Valid commands are listed in commands."""

lirc.py
@@ -12,7 +12,8 @@ Also does some parsing of the lircd.conf file to find available codes."""
     codes = {}
 
     def __init__(self, conf="/etc/lirc/lircd.conf"):
-        self._parse(conf)
+        pass
+#        self._parse(conf)
 
     def _parse(self, conf):
         """Parse the lircd.conf config file and create a dictionary.

commit 9da02ea9dccfba1959a37b97c75579132d7afa62

Back out of debugging changes that broke IR
In commit 00234e4e2e31452c4ff1c74d2e786b03707691e3 I accidently checked
in some debugging code that disabled the LIRC commands along with the
new icons. Back out of those unintentional changes.

eccontrols/generic.py
@@ -11,10 +11,10 @@ class GenericIR:
     commands = {}
 
     def __init__(self, device):
-        # if not device in lirc.devices():
-        #     raise ECError('No such device: %s' % device)
+        if not device in lirc.devices():
+            raise ECError('No such device: %s' % device)
         self.device = device
-        # self.commands = lirc.commands(device)
+        self.commands = lirc.commands(device)
 
     def send(self, command):
         """Send an IR code. Valid commands are listed in commands."""

lirc.py
@@ -12,8 +12,7 @@ Also does some parsing of the lircd.conf file to find available codes."""
     codes = {}
 
     def __init__(self, conf="/etc/lirc/lircd.conf"):
-        pass
-#        self._parse(conf)
+        self._parse(conf)
 
     def _parse(self, conf):
         """Parse the lircd.conf config file and create a dictionary.