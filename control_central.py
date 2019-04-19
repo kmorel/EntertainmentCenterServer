92779a083276cbec2facd3881656dffd4532f6f0
Kenneth Moreland <kmorel@sandia.gov>
2019-04-18 21:07:57 -0600

    Switch back to socket connection for the receiver
    I have the network set back up for that ethernet device.

--- control_central.py
+++ control_central.py
@@ -34,7 +34,7 @@ class ControlCentral:
 
     def __init__(self):
         self._bluray = generic.GenericIR('SonyBluRay')
-        self._receiver = pioneer.ReceiverIR()
+        self._receiver = pioneer.ReceiverSocket()
         self._directv = generic.GenericIR('DirecTV', duration=1)
         self._tv = generic.GenericIR('SonyTV')
         # Disabling because this is not working right now and current setup is
